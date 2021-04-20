b38f8f5833f742491d3d32dd696f5379e8bb618f
AntonTuzov <antontuzovf@gmail.com>
2021-04-20 10:57:06 +0300

    Initial Commit

---
+++ WeatherApp SwiftUI/WeatherApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftUIApp.swift
+//  WeatherApp SwiftUI
+//
+//  Created by turbo on 20.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
